dc081bafb610b90d2f5412d1098b2eb833979549
chloegg <163141867+chloegg@users.noreply.github.com>
2024-10-25 01:11:26 -0400

    week7

---
+++ week7/Week07/Week07/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Week07
+//
+//  Created by Keying Guo on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
