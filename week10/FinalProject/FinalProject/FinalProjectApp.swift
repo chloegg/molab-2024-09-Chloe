79e91b3fa2f37f62c35bd3033c88d8c464004c4b
chloegg <163141867+chloegg@users.noreply.github.com>
2024-11-08 11:25:30 -0500

    week10

---
+++ week10/FinalProject/FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Keying Guo on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
